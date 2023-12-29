453efcd7ebee9990bdd44dcb7e5d690f6e007f38
yurix <yuraggwp@gmail.com>
2023-12-29 16:25:27 +0200

    Added schedule and users apps to INSTALLED_APPS

--- gradebook/settings.py
+++ gradebook/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gradebook',
+    'schedule.apps.ScheduleConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
