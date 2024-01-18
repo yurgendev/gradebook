5f40b3a09d873038ec8abb4b24a06783ed5af536
yurix <yuraggwp@gmail.com>
2024-01-18 15:17:44 +0200

    чутка обновил тесты и перезалил бд потому что что-то сломал и запутался  когда распутался все перепроверил что модели бзеров работают и продолжаю

--- users/tests.py
+++ users/tests.py
@@ -1,15 +1,34 @@
 from django.test import TestCase
 from .models import Teacher, Parent, Student
 
+
 class UsersModelsTest(TestCase):
     def test_teacher_str(self):
-        teacher = Teacher.objects.create(first_name='Oksana', last_name='Yanchenko', middle_name='Yuriievna', email='op@ukraine.com')
-        self.assertEqual(str(teacher), 'Yanchenko Oksana Yuriievna')
+        teacher = Teacher.objects.create(
+            first_name='Oksana',
+            last_name='Yanchenko',
+            middle_name='Yuriievna',
+            email='op@ukraine.com'
+        )
+
+        self.assertEqual(
+            str(teacher),
+            'Yanchenko Oksana Yuriievna'
+        )
 
     def test_parent_str(self):
-        parent = Parent.objects.create(first_name='Anatolii', last_name='Ivanov', middle_name='', email='anatol@example.com')
+        parent = Parent.objects.create(
+            first_name='Anatolii',
+            last_name='Ivanov',
+            middle_name='',
+            email='anatol@example.com'
+        )
         self.assertEqual(str(parent), 'Ivanov Anatolii')
 
     def test_student_str(self):
-        student = Student.objects.create(first_name='Vlad', last_name='Saloevskii', email='vlad@example.com')
+        student = Student.objects.create(
+            first_name='Vlad',
+            last_name='Saloevskii',
+            email='vlad@example.com'
+        )
         self.assertEqual(str(student), 'Saloevskii Vlad')
