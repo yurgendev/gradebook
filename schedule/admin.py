995cdc2e58f85a9d56f5351462503b43a3d72de7
yurix <yuraggwp@gmail.com>
2024-02-09 13:32:03 +0200

    admin.py for users and schedule updated with decorator and list_display/list_editable for readable

--- schedule/admin.py
+++ schedule/admin.py
@@ -2,15 +2,42 @@ from django.contrib import admin
 from .models import SchoolClass, Lesson, Comment, Grade, CalendarEvent
 
 
-
 class SchoolClassAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         return qs.prefetch_related('students').order_by('students__last_name', 'students__first_name')
 
 
-admin.site.register(SchoolClass, SchoolClassAdmin)
-admin.site.register(Lesson)
-admin.site.register(Comment)
-admin.site.register(Grade)
-admin.site.register(CalendarEvent)
+@admin.register(SchoolClass)
+class SchoolClassAdmin(admin.ModelAdmin):
+    list_display = ('class_name',)
+    list_editable = ('class_name',)
+    list_display_links = None
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('lesson_date', 'teacher', 'subject')
+    list_editable = ('teacher', 'subject')
+    list_display_links = ('lesson_date',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text', 'created_at', 'author')
+    list_editable = ('author',)
+    list_display_links = ('text',)
+
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ('value', 'student', 'lesson', 'grade_type')
+    list_editable = ('student', 'lesson', 'grade_type')
+    list_display_links = ('value',)
+
+
+@admin.register(CalendarEvent)
+class CalendarEventAdmin(admin.ModelAdmin):
+    list_display = ('event_date', 'grade')
+    list_editable = ('grade',)
+    list_display_links = ('event_date',)
--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,30 @@
 from django.contrib import admin
-
-# Register your models here.
-
-
 from .models import Person, Teacher, Parent, Student
 
-admin.site.register(Person)
-admin.site.register(Teacher)
-admin.site.register(Parent)
-admin.site.register(Student)
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    list_editable = ('email',)
+    list_display_links = ('first_name', 'last_name')
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    list_editable = ('email',)
+    list_display_links = ('first_name', 'last_name')
+
+
+@admin.register(Parent)
+class ParentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    list_editable = ('email',)
+    list_display_links = ('first_name', 'last_name')
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'date_of_birth')
+    list_editable = ('email', 'date_of_birth')
+    list_display_links = ('first_name', 'last_name')
